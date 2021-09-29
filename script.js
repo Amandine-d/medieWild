7f9bb0442a8dad13b8dbd5829b12c7d00e3bdb62
Peter Joachym <joachym.peter@gmail.com>
2021-09-29 15:40:44 +0200

    paper, rock, scisors buttons fils array player choice

--- script.js
+++ script.js
@@ -9,7 +9,23 @@ to do list:
 let sandraChoice = Math.floor(Math.random() * 3) + 1;
 let playerChoice = [];
 
+const choice = document.querySelectorAll(".player-choice");
+
 const paper = "1";
 const rock = "2";
 const scisors = "3";
 
+//click and touch event choice
+for (let i = 0; i < choice.length; i++) {
+  choice[i].addEventListener("click", function () {
+    playerChoice = [];
+    playerChoice.push(choice[i].value);
+    console.log(playerChoice);
+  });
+}
+
+// function that chose the winner
+
+// function chooseTheWinner(){
+//     if()
+// }
