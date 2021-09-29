744db6ac34563b3c11400b19d91093eadde596d3
Peter Joachym <joachym.peter@gmail.com>
2021-09-29 14:48:22 +0200

    index.js created, commmentare to do list added

---
+++ script.js
@@ -0,0 +1,9 @@
+/* chifoumi 
+to do list: 
+- create empty arrays for 
+            -playerChoice
+            -sandraChoice 
+- create the function to show a random number  
+- create the function to make a link with shifumi assets
+- create the function that compare playerChoice array and sandraChoice and chose the winner
+- create the function that count amount of games won */ 
