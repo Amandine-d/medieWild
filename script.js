29f992dff84b3584bfc0372625cc2558584a7b4d
Peter Joachym <joachym.peter@gmail.com>
2021-09-29 14:58:45 +0200

    Arrays and const sandraChoice(random) created

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 /* chifoumi 
 to do list: 
-- create empty arrays for 
-            -playerChoice
-            -sandraChoice 
-- create the function to show a random number  
+- create empty arrays for playerChoice /done
+- create a const sandraChoice that show a random number between 1 and 3/ done  
 - create the function to make a link with shifumi assets
 - create the function that compare playerChoice array and sandraChoice and chose the winner
-- create the function that count amount of games won */ 
+- create the function that count amount of games won */
+let sandraChoice = Math.floor(Math.random() * 3) + 1;
+let playerChoice = [];
+
+function randomNumber() {}
