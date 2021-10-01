610afcc26c51d403ac9a994adffb013e23a733ab
Fanny <fanny.lagadec.pro@gmail.com>
2021-10-01 16:39:26 +0200

    begin trying media query  dojo mobi

--- dojo.js
+++ dojo.js
@@ -1,28 +1,37 @@
+const result = document.getElementById("result");
+//result.readOnly = true; //empeche d'ecrire ds mon input resultats
+
+//function choisir un nombre entier random
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
+//ciblage de tous les boutons
 playButtons = document.getElementsByClassName("play");
 
+//choisir un nombre aleatoire entre 1 et 6
 let randomChoice = getRandomIntInclusive(1, 6);
 console.log(randomChoice);
-let userChoice;
+let userChoice; //créer la boite userChoice
 
+//loop pour listen le clic sur tous les boutons
 for (let i = 0; i < playButtons.length; i++) {
   playButtons[i].addEventListener("click", function () {
-    userChoice = playButtons[i].value;
+    userChoice = playButtons[i].value; //quand clic->stock la valeur du bouton ds la boite user
     console.log(userChoice);
   });
 
+  //quand clic compare userchoice et randomchoice
   playButtons[i].addEventListener("click", function compare() {
     if (randomChoice == userChoice) {
-      console.log("yataaa");
+      result.innerHTML="How did you know?";
       randomChoice = getRandomIntInclusive(1, 6);
+      //si identique, gagné et choisi un nouveau random number
       console.log(randomChoice);
     } else {
-      console.log("try again");
+      result.innerHTML="Try again";
+      //si perdu rien ne se passe
     }
   });
 }
