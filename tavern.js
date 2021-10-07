44fa25403fc5ce876d6f77489040a6e7959219fc
Amandine <84130470+Amandine-d@users.noreply.github.com>
2021-10-07 17:33:43 +0200

    added audio

--- tavern.js
+++ tavern.js
@@ -303,11 +303,13 @@ function battleMessage() {
 }
 
 /* footer pierre*/
+const audio = new Audio('assets/scream.mp3');
 const bubbleContainer = document.querySelector('.nav-pierre');
 
 const menuPierre = document.querySelector('.floating-head')
 
 menuPierre.addEventListener('click', function() {
     bubbleContainer.classList.toggle('active');
+    audio.play()
 
-})
+});
