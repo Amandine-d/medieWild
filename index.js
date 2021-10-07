2fb61ef8cbf238a2e73d4838f247998608d3607b
Amandine <84130470+Amandine-d@users.noreply.github.com>
2021-10-07 17:32:20 +0200

    added audio

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const audio = new Audio('assets/scream.mp3');
 const bubbleContainer = document.querySelector('.nav-pierre');
 
 const menuPierre = document.querySelector('.floating-head')
 
 menuPierre.addEventListener('click', function() {
     bubbleContainer.classList.toggle('active');
+    audio.play()
 
-})
+});
