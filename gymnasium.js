e45e8298c47491e9c9272a72aad0be1b345d9548
Amandine <84130470+Amandine-d@users.noreply.github.com>
2021-10-07 17:31:54 +0200

    added audio

--- gymnasium.js
+++ gymnasium.js
@@ -240,11 +240,13 @@ const questions = [
 
   /*footer pierre*/
 
-  const bubbleContainer = document.querySelector('.nav-pierre');
+ const audio = new Audio('assets/scream.mp3');
+const bubbleContainer = document.querySelector('.nav-pierre');
 
-  const menuPierre = document.querySelector('.floating-head')
-  
-  menuPierre.addEventListener('click', function() {
-      bubbleContainer.classList.toggle('active');
-  
-  })
+const menuPierre = document.querySelector('.floating-head')
+
+menuPierre.addEventListener('click', function() {
+    bubbleContainer.classList.toggle('active');
+    audio.play()
+
+});
