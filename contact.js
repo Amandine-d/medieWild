f60bd793f3826cc11a2c2e2e9c83649965331d7d
Amandine <84130470+Amandine-d@users.noreply.github.com>
2021-10-08 11:09:15 +0200

    added footer pierre

--- contact.js
+++ contact.js
@@ -7,3 +7,16 @@ formContact.addEventListener('submit', (e) => {
     audioSent.play()
     console.log('form submitted')
 })
+
+  /*footer pierre*/
+
+ const audio = new Audio('assets/scream.mp3');
+const bubbleContainer = document.querySelector('.nav-pierre');
+
+const menuPierre = document.querySelector('.floating-head')
+
+menuPierre.addEventListener('click', function() {
+    bubbleContainer.classList.toggle('active');
+    audio.play()
+
+});
